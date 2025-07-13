ac91bd6f8713aed8bd3cc3f6798e2f40dd5a79b2
itscrisu <cristian.eng3@gmail.com>
2025-07-13 02:59:23 -0300

    add more files + use sys module to allow multiple path search

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from stats import get_num_words
-from stats import count_characters
-from stats import sort_characters
+import sys
+
+from stats import get_num_words, count_characters, sort_characters
 
 def get_book_text(filepath):
   with open(filepath) as file:
     return file.read()
 
 def main():
-  text = get_book_text("books/frankenstein.txt")
+  if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  
+  path = sys.argv[1]
+  text = get_book_text(path)
 
   word_count = get_num_words(text)
   char_counts = count_characters(text)
@@ -19,7 +24,7 @@ def main():
 # Found 75767 total words
 # --------- Character Count -------
   print("============ BOOKBOT ============")
-  print(f"Analyzing book found at books/frankenstein.txt...")
+  print(f"Analyzing book found at {path}...")
   print("----------- Word Count ----------")
   print(f"Found {word_count} total words")
   print("--------- Character Count -------")
